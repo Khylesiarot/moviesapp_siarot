13b5d703f6c4aaaba28fd73611ad6b6835d5d8db
khyle_siarot <johnkhyle.siarot.20@usjr.edu.ph>
2023-02-24 19:56:34 +0800

    first commit

---
+++ lib/Screens/onboardingscreen.dart
@@ -0,0 +1,22 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:moviesapp_siarot/constants.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor: Constants.kBlackColor,
+      extendBody: true,
+      body: SizedBox(
+        
+      ),
+    );
+  }
+}
---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  // Color
+  static const Color kBlackColor = Color(0xFF19191B);
+  
+}
