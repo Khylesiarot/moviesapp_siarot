a7f22bca86577bcd5fc0b8a433527d8f306d2777
khyle_siarot <johnkhyle.siarot.20@usjr.edu.ph>
2023-02-24 22:25:00 +0800

    circle blur background

--- lib/Screens/onboardingscreen.dart
+++ lib/Screens/onboardingscreen.dart
@@ -12,10 +12,53 @@ class OnboardingScreen extends StatelessWidget {
     final screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      backgroundColor: Constants.kBlackColor,
+      backgroundColor: Constants.blck,
       extendBody: true,
       body: SizedBox(
-        
+        width: screenWidth,
+        height: screenHeight,
+        child: Stack(
+          children: [Positioned(
+            top: screenHeight * .1,
+            left: screenWidth * .12,
+            child: Container(
+              height: 166,
+              width: 166,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Constants.pnk,
+              ),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(
+                sigmaX: 75,
+                sigmaY: 75,              ),
+              child: Container(
+                height: 166,
+                width: 166,
+                color: Colors.transparent,
+              ), ),
+          )),
+          Positioned(
+            top: screenHeight * .4,
+            left: screenWidth * .55,
+            child: Container(
+              height: 200,
+              width: 200,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Constants.grn,
+              ),
+              child: BackdropFilter(filter: ImageFilter.blur(
+                sigmaX: 80,
+                sigmaY: 80,
+              ),
+              child: Container(
+                height: 200,
+                width: 200,
+                color: Colors.transparent,
+              ), ),
+          ))],
+        ),
       ),
     );
   }
--- lib/constants.dart
+++ lib/constants.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 class Constants {
   // Color
-  static const Color kBlackColor = Color(0xFF19191B);
+  static const Color blck = Color(0xFF19191B);
+  static const Color pnk = Color(0xFFFE53BB);
+  static const Color grn = Color(0xFF09FBD3);
+  
   
 }
